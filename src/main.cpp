13cbc482d1fe0234ff44003f041d73c4be266209
Cee-Ry <eduaveceasar@gmail.com>
2026-06-19 15:08:52 +0800

    fix: make the menu and playing conditional

--- src/main.cpp
+++ src/main.cpp
@@ -14,33 +14,37 @@ int main() {
   Texture2D title {LoadTexture("assets/textures/title/title_board.png")};
   Texture2D bird {LoadTexture("assets/textures/player/bird.png")};
 
+  bool inGame {false};
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawTexturePro(bg_img,  // background image
-      (Rectangle) {0, 0, (float)bg_img.width, (float)bg_img.height},
-      (Rectangle) {0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
-      (Vector2) {0, 0},
-      0.0f,
-      WHITE
-    );
 
-    DrawTexturePro(title, // title with board
-      (Rectangle) {0, 0, (float)title.width, (float)title.height},
-      (Rectangle) {(float)(GetScreenWidth() / 2) - (title.width / 2), 0, (float)title.width, (float)title.height},
-      (Vector2) {0, 0},
-      0.0f,
-      WHITE
-    );
+    if (inGame) {
+      DrawTexturePro(bg_img,  // background image
+        (Rectangle) {0, 0, (float)bg_img.width, (float)bg_img.height},
+        (Rectangle) {0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
+        (Vector2) {0, 0},
+        0.0f,
+        WHITE
+      );
 
-    DrawTexturePro(bird, // bird character or the menu mascot
-      (Rectangle) {0, 0, (float)bird.width, (float)bird.height},
-      (Rectangle) {(float)(GetScreenWidth() / 2) - (bird.width / 2), (float)(GetScreenHeight() / 2) - (bird.height / 2), (float)bird.width, (float)bird.height},
-      (Vector2) {0, 0},
-      20.0f,
-      WHITE
-    );
+      DrawTexturePro(title, // title with board
+        (Rectangle) {0, 0, (float)title.width, (float)title.height},
+        (Rectangle) {(float)(GetScreenWidth() / 2) - (title.width / 2), 0, (float)title.width, (float)title.height},
+        (Vector2) {0, 0},
+        0.0f,
+        WHITE
+      );
 
+      DrawTexturePro(bird, // bird character or the menu mascot
+        (Rectangle) {0, 0, (float)bird.width, (float)bird.height},
+        (Rectangle) {(float)(GetScreenWidth() / 2) - (bird.width / 2), (float)(GetScreenHeight() / 2) - (bird.height / 2), (float)bird.width, (float)bird.height},
+        (Vector2) {0, 0},
+        20.0f,
+        WHITE
+      );
+    }
     EndDrawing();
   }
 
